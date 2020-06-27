d39fa0c77d31bb31d6bbd680e5f1cdb694d36126
SidK <sidcool1234@gmail.com>
2020-06-27 10:19:48 +0530

    Refactors Test classes to reduce object creation duplication]

--- src/test/java/com/sid/robotsimulation/model/PositionTest.java
+++ src/test/java/com/sid/robotsimulation/model/PositionTest.java
@@ -11,30 +11,34 @@ class PositionTest {
 
     @BeforeEach
     private void initializePosition(){
-        position = new Position(1,1);
+        position = positonObject(1, 1);
+    }
+
+    private static Position positonObject(int i, int i2) {
+        return new Position(i, i2);
     }
 
     @Test
     void moveLeft() {
         Position leftPosition = position.left();
-        assertEquals(new Position(0,1), leftPosition);
+        assertEquals(positonObject(0, 1), leftPosition);
     }
 
     @Test
     void moveRight() {
         Position rightPosition = position.right();
-        assertEquals(new Position(2,1), rightPosition);
+        assertEquals(positonObject(2, 1), rightPosition);
     }
 
     @Test
     void moveUp() {
         Position upPosition = position.up();
-        assertEquals(new Position(1,2), upPosition);
+        assertEquals(positonObject(1, 2), upPosition);
     }
 
     @Test
     void moveDown() {
         Position downPosition = position.down();
-        assertEquals(new Position(1,0), downPosition);
+        assertEquals(positonObject(1, 0), downPosition);
     }
 }
--- src/test/java/com/sid/robotsimulation/model/RobotTest.java
+++ src/test/java/com/sid/robotsimulation/model/RobotTest.java
@@ -6,44 +6,48 @@ import org.junit.jupiter.api.Test;
 class RobotTest {
     @Test
     public void shouldCreateRobotWithPosition(){
-        Robot robot = new Robot(new Position(0,0));
+        Robot robot = new Robot(positionObject(0, 0));
 
-        Assertions.assertEquals(new Position(0, 0), robot.getCurrentPosition());
+        Assertions.assertEquals(positionObject(0, 0), robot.getCurrentPosition());
+    }
+
+    private static Position positionObject(int i, int i2) {
+        return new Position(i, i2);
     }
 
     @Test
     public void shouldMoveTheRobotUp(){
-        Robot robot = new Robot(new Position(0,0));
+        Robot robot = new Robot(positionObject(0, 0));
 
         robot.moveUp();
 
-        Assertions.assertEquals(new Position(0, 1), robot.getCurrentPosition());
+        Assertions.assertEquals(positionObject(0, 1), robot.getCurrentPosition());
     }
 
     @Test
     public void shouldMoveTheRobotDown(){
-        Robot robot = new Robot(new Position(0,1));
+        Robot robot = new Robot(positionObject(0, 1));
 
         robot.moveDown();
 
-        Assertions.assertEquals(new Position(0, 0), robot.getCurrentPosition());
+        Assertions.assertEquals(positionObject(0, 0), robot.getCurrentPosition());
     }
 
     @Test
     public void shouldMoveTheRobotRight(){
-        Robot robot = new Robot(new Position(1,0));
+        Robot robot = new Robot(positionObject(1, 0));
 
         robot.moveRight();
 
-        Assertions.assertEquals(new Position(2, 0), robot.getCurrentPosition());
+        Assertions.assertEquals(positionObject(2, 0), robot.getCurrentPosition());
     }
 
     @Test
     public void shouldMoveTheRobotLeft(){
-        Robot robot = new Robot(new Position(1,0));
+        Robot robot = new Robot(positionObject(1, 0));
 
         robot.moveLeft();
 
-        Assertions.assertEquals(new Position(0, 0), robot.getCurrentPosition());
+        Assertions.assertEquals(positionObject(0, 0), robot.getCurrentPosition());
     }
 }
